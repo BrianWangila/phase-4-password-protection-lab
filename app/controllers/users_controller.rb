28782b735d63828192c48a34c9196d98c64b7a59
Brian Wangila <bwanjalah@gmail.com>
2022-10-07 15:48:52 +0300

    commit tree 1e4171f8fa94e6cd5096b05c914bc859b1cf7462

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :authorize, only: [:show]
+
+  def create
+    user = User.create(signup_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user, status: :ok
+    else
+      render json: {error: "Not found"}, status: :not_found
+    end
+  end
+
+  
+  private
+
+  def authorize
+    return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+  end
+
+  def signup_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
